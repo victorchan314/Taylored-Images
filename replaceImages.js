8a3070fae0ec6a5dde955d57e5b405b7f62437db
Victor Chan <victorchan88888888@gmail.com>
2017-06-25 23:56:24 -0700

    Updated manifest and replaceImages

--- replaceImages.js
+++ replaceImages.js
@@ -2,15 +2,11 @@ function convert(s) {
     return s.replace(/university of california, berkeley|uc berkeley|ucberkeley|berkeley| cal /gi, "The Number One Public University in the World");
 }
 
-function replaceText(element) {
-    var elements = element.childNodes;
-    for (var i=0; i<elements.length; i++) {
-        if (elements[i].nodeType == 3) {
-            elements[i].textContent = convert(elements[i].textContent);
-        } else {
-            replaceText(elements[i]);
-        }
+function replaceImages() {
+    var imgs = document.getElementsByTagName("img");
+    for (var i=0; i<imgs.length; i++) {
+        imgs[i].src = "http://az616578.vo.msecnd.net/files/2016/06/09/636010490078615374-141787448_cover.jpg";
     }
 }
 
-window.onload = replaceText(document.body);
+window.onload = replaceImages();
