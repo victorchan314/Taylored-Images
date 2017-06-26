d037618a71206505958a2b13f8709d2a81ef7f45
Victor Chan <victorchan88888888@gmail.com>
2017-06-25 23:57:49 -0700

    Created separate function to find image of Taylor Swift

--- replaceImages.js
+++ replaceImages.js
@@ -1,11 +1,11 @@
-function convert(s) {
-    return s.replace(/university of california, berkeley|uc berkeley|ucberkeley|berkeley| cal /gi, "The Number One Public University in the World");
+function findImage(i) {
+    return "http://az616578.vo.msecnd.net/files/2016/06/09/636010490078615374-141787448_cover.jpg"
 }
 
 function replaceImages() {
     var imgs = document.getElementsByTagName("img");
     for (var i=0; i<imgs.length; i++) {
-        imgs[i].src = "http://az616578.vo.msecnd.net/files/2016/06/09/636010490078615374-141787448_cover.jpg";
+        imgs[i].src = findImage(imgs[i]);
     }
 }
 
