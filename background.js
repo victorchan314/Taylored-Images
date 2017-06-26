6035628a81d3c744eb7e95c54335e29cb0d0e113
Victor Chan <victorchan88888888@gmail.com>
2017-06-25 23:50:21 -0700

    Updated background.js to cal the correct function

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({code: 'replaceText(document.body);'});
+    chrome.tabs.executeScript({code: 'replaceImages();'});
 });
