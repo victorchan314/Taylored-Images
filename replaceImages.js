e974a9fa19064c3cd5b41d57ba07f4829c1599a3
Victor Chan <victorchan88888888@gmail.com>
2017-06-27 23:02:34 -0700

    Finished jQuery function to get images...unfortunately am a victim of Same Origin Policy

--- replaceImages.js
+++ replaceImages.js
@@ -1,8 +1,15 @@
 function findImage(i) {
     $.get("https://www.google.com/search?hl=en&site=imghp&tbm=isch&source=hp&biw=1280&bih=620&q=taylor+swift+imagesize%3D" + i.width + "x" + i.height + "&gs_l=img.3...32.3729.0.3889.30.27.0.0.0.0.151.1909.21j5.26.0....0...1.1.64.img..4.16.1206...0.5Q6eknT9PLw", function(data, status) {
-        return $("img")[0].src;
+        var images = $("img");
+        for (var i = 0; i < images.length; i++) {
+            var s = images[i].src;
+            console.log(s);
+            if (s.startsWith("data")) {
+                alert(s);
+            }
+        }
+        return "http://az616578.vo.msecnd.net/files/2016/06/09/636010490078615374-141787448_cover.jpg"
     });
-    //return "http://az616578.vo.msecnd.net/files/2016/06/09/636010490078615374-141787448_cover.jpg"
 }
 
 function replaceImages() {
