e5dd03d4a153d11359f5fef19fd7b57277b29748
Victor Chan <victorchan88888888@gmail.com>
2017-07-16 01:46:45 -0700

    Changed repeated variable name

--- replaceImages.js
+++ replaceImages.js
@@ -1,5 +1,5 @@
-function findImage(i) {
-    $.get("https://www.google.com/search?hl=en&site=imghp&tbm=isch&source=hp&biw=1280&bih=620&q=taylor+swift+imagesize%3D" + i.width + "x" + i.height + "&gs_l=img.3...32.3729.0.3889.30.27.0.0.0.0.151.1909.21j5.26.0....0...1.1.64.img..4.16.1206...0.5Q6eknT9PLw", function(data, status) {
+function findImage(img) {
+    $.get("https://www.google.com/search?hl=en&site=imghp&tbm=isch&source=hp&biw=1280&bih=620&q=taylor+swift+imagesize%3D" + img.width + "x" + img.height + "&gs_l=img.3...32.3729.0.3889.30.27.0.0.0.0.151.1909.21j5.26.0....0...1.1.64.img..4.16.1206...0.5Q6eknT9PLw", function(data, status) {
         var images = $("img");
         for (var i = 0; i < images.length; i++) {
             var s = images[i].src;
