6b4fb08ece1e05654c6d81fb2bb029e5aa494307
Victor Chan <victorchan88888888@gmail.com>
2017-07-16 03:31:11 -0700

    Added API key and search engine ID to ignored file :)

--- replaceImages.js
+++ replaceImages.js
@@ -1,5 +1,5 @@
 function findImage(img) {
-    $.getJSON("https://www.googleapis.com/customsearch/v1?q=Taylor+Swift&cx=" + config.SEARCH_ENGINE_ID + "&num=1&searchType=image&key=" + config.API_KEY, function(data) {
+    $.getJSON("https://www.googleapis.com/customsearch/v1?q=Taylor+Swift&cx=" + keys.SEARCH_ENGINE_ID + "&num=1&searchType=image&key=" + keys.API_KEY, function(data) {
         console.log(data.items[0].link);
         //return data.items[0].link;
     });
