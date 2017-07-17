2d170676e090f8fa450bd052aa75c6885b6f416f
Victor Chan <victorchan88888888@gmail.com>
2017-07-16 23:26:20 -0700

    Created function to determine best image size to search for in order to find closer images of Taylor

--- replaceImages.js
+++ replaceImages.js
@@ -1,7 +1,14 @@
 function findImage(img) {
-    $.getJSON("https://www.googleapis.com/customsearch/v1?q=Taylor+Swift&cx=" + keys.SEARCH_ENGINE_ID + "&num=10&searchType=image&key=" + keys.API_KEY, function(data) {
+    $.getJSON("https://www.googleapis.com/customsearch/v1?q=Taylor+Swift+imagesize:" + specifySize(img) + "&cx=" + keys.SEARCH_ENGINE_ID + "&num=10&searchType=image&key=" + keys.API_KEY, function(data) {
         return data.items[Math.floor(10*Math.random())].link;
     });
+}
+
+function specifySize(i) {
+    function ssr(x, y) {
+        return Math.sqrt(Math.pow(i.width-x, 2) + Math.pow(i.height-y, 2));
+    }
+    if ssr(
 }
 
 function replaceImages() {
