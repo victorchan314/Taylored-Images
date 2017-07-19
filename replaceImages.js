402a31871533c918e3ef0103a515c0345efef191
Victor Chan <victorchan88888888@gmail.com>
2017-07-19 00:18:35 -0700

    Finished function to crop images to retain original sizes

--- replaceImages.js
+++ replaceImages.js
@@ -1,12 +1,26 @@
 function findImage(img) {
     var w = img.width;
     var h = img.height;
-    $.getJSON("https://www.googleapis.com/customsearch/v1?q=Taylor+Swift+imagesize:" + specifySize(img) + "&cx=" + keys.SEARCH_ENGINE_ID + "&num=10&searchType=image&key=" + keys.API_KEY,
-            function(data) {
-                img.src = data.items[Math.floor(10*Math.random())].link;
+    $.getJSON(
+        "https://www.googleapis.com/customsearch/v1?q=Taylor+Swift+imagesize:" + specifySize(img) + "&cx=" + keys.SEARCH_ENGINE_ID + "&num=10&searchType=image&key=" + keys.API_KEY,
+        function(data) {
+            var d = document.createElement("div");
+            d.style.cssText = "width: " + w + "px; height: " + h + "px; overflow: hidden;";
+
+            img.src = data.items[Math.floor(10*Math.random())].link;
+            if (img.width / img.height <= w / h) {
                 img.width = w;
+                var diff = Math.floor((img.height - h) / 2);
+                img.style["margin-top"] = -Math.floor((img.height - h) / 2);
+            } else {
                 img.height = h;
-            });
+                img.style["margin-left"] = -Math.floor((img.width - w) / 2);
+            }
+
+            var p = img.parentNode;
+            p.replaceChild(d, img);
+            d.appendChild(img);
+    });
 }
 
 function specifySize(img) {
@@ -41,4 +55,5 @@ ratios[5 / 4] = "1280x1024";
 ratios[4 / 3] = "1920x1440";
 ratios[8 / 5] = "3840x2400";
 ratios[16 / 9] = "1920x1080";
+
 window.onload = replaceImages();
